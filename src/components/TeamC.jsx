64fd9ae2269d9af2c6758b5527e3a168f8131701
Xander Bezanson <w0468734@nscc.ca>
2026-08-09 02:12:44 -0300

    small changes for Hangout Zone title on site and for Team C Studios discord link

--- src/components/TeamC.jsx
+++ src/components/TeamC.jsx
@@ -14,6 +14,9 @@ const TeamC = () => {
                 <div className="text-lg">
                     Team C Studios is an indie game development group created by XannyXu, Frank_the_Ogre, and their friends.
                 </div>
+                <div className="text-lg">
+                    Here is the Team C Studios invite link: <a className="text-blue-700 visited:text-purple-700" target="blank" href={"https://discord.gg/ucPcb3JB9F"}>Team C Studios Discord</a>
+                </div>
             </div>
             <Footer/>
         </div>
